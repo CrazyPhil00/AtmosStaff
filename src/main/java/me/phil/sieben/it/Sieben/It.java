6468a108ae97694a0ea8694d658e3029927040f9
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 20:52:17 +0200

    Initial commit

---
+++ src/main/java/me/phil/sieben/it/Sieben/It.java
@@ -0,0 +1,17 @@
+package me.phil.sieben.it.Sieben;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class It extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
